ec5804f1a4faf1c33c7e30ff2a550f896aeaecb7
arunkumar2746 <53178319+arunkumar2746@users.noreply.github.com>
2020-01-29 20:09:38 +0530

    Initial commit

---
+++ helloworldp2.java
@@ -0,0 +1,11 @@
+class A {
+ public static void main(String args[]){
+     System.out.println("Hello World is my first jenkins code ");
+System.out.println("Hello Hari ");
+System.out.println("Hello Kumar ");
+System.out.println("Hello Suresh ");
+System.out.println("Hello Kumar ");
+
+
+ }
+}
